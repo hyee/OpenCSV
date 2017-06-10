d1b5cc324646c5863a8af3587db83a72102b6032
hyee <hyeetsc@gmail.com>
2017-06-10 16:09:10 +0800

    bug fix

--- src/com/opencsv/RowHandler.java
+++ src/com/opencsv/RowHandler.java
@@ -1,10 +1,7 @@
 package com.opencsv;
 
-import com.lmax.disruptor.EventHandler;
-import com.lmax.disruptor.LifecycleAware;
+import com.lmax.disruptor.*;
 
-/**
- * Created by Will on 2017/5/27.
- */
-public interface RowHandler<T> extends EventHandler<T>, LifecycleAware {
+
+public abstract  interface RowHandler<T> extends EventHandler<T>,LifecycleAware {
 }
