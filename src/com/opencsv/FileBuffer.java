9c5a64e4dc310f835698d9d747044031a8972b25
hyee <hyeetsc@gmail.com>
2023-02-26 15:14:29 +0800

    fix issue #7

--- src/com/opencsv/FileBuffer.java
+++ src/com/opencsv/FileBuffer.java
@@ -1,13 +1,15 @@
 package com.opencsv;
 
-import sun.nio.ch.DirectBuffer;
-
 import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
@@ -148,6 +150,97 @@ public class FileBuffer implements Closeable {
         return flush(false);
     }
 
+    private static boolean PRE_JAVA_9 = System.getProperty("java.specification.version","9").startsWith("1.");
+    private static Method cleanMethod;
+    private static Method attachmentMethod;
+    private static Object theUnsafe;
+
+    //ref: https://stackoverflow.com/questions/2972986/how-to-unmap-a-file-from-memory-mapped-using-filechannel-in-java/54046774#54046774
+    static void getCleanMethodPrivileged() {
+        if (PRE_JAVA_9) {
+            try {
+                cleanMethod = Class.forName("sun.misc.Cleaner").getMethod("clean");
+                cleanMethod.setAccessible(true);
+                final Class<?> directByteBufferClass =
+                        Class.forName("sun.nio.ch.DirectBuffer");
+                attachmentMethod = directByteBufferClass.getMethod("attachment");
+                attachmentMethod.setAccessible(true);
+            } catch (final Exception ex) {
+            }
+        } else {
+            try {
+                Class<?> unsafeClass;
+                try {
+                    unsafeClass = Class.forName("sun.misc.Unsafe");
+                } catch (Exception e) {
+                    // jdk.internal.misc.Unsafe doesn't yet have invokeCleaner(),
+                    // but that method should be added if sun.misc.Unsafe is removed.
+                    unsafeClass = Class.forName("jdk.internal.misc.Unsafe");
+                }
+                cleanMethod = unsafeClass.getMethod("invokeCleaner", ByteBuffer.class);
+                cleanMethod.setAccessible(true);
+                final Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
+                theUnsafeField.setAccessible(true);
+                theUnsafe = theUnsafeField.get(null);
+            } catch (final Exception ex) {
+            }
+        }
+    }
+
+    static {
+        AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            @Override
+            public Object run() {
+                getCleanMethodPrivileged();
+                return null;
+            }
+        });
+    }
+
+    private static boolean closeDirectByteBufferPrivileged(
+            final ByteBuffer byteBuffer) {
+        try {
+            if (cleanMethod == null) {
+                return false;
+            }
+            if (PRE_JAVA_9) {
+                if (attachmentMethod == null) {
+                    return false;
+                }
+                // Make sure duplicates and slices are not cleaned, since this can result in
+                // duplicate attempts to clean the same buffer, which trigger a crash with:
+                // "A fatal error has been detected by the Java Runtime Environment:
+                // EXCEPTION_ACCESS_VIOLATION"
+                // See: https://stackoverflow.com/a/31592947/3950982
+                if (attachmentMethod.invoke(byteBuffer) != null) {
+                    // Buffer is a duplicate or slice
+                    return false;
+                }
+                // Invoke ((DirectBuffer) byteBuffer).cleaner().clean()
+                final Method cleaner = byteBuffer.getClass().getMethod("cleaner");
+                cleaner.setAccessible(true);
+                cleanMethod.invoke(cleaner.invoke(byteBuffer));
+                return true;
+            } else {
+                if (theUnsafe == null) {
+                    return false;
+                }
+                // In JDK9+, calling the above code gives a reflection warning on stderr,
+                // need to call Unsafe.theUnsafe.invokeCleaner(byteBuffer) , which makes
+                // the same call, but does not print the reflection warning.
+                try {
+                    cleanMethod.invoke(theUnsafe, byteBuffer);
+                    return true;
+                } catch (final IllegalArgumentException e) {
+                    // Buffer is a duplicate or slice
+                    return false;
+                }
+            }
+        } catch (final Exception e) {
+            return false;
+        }
+    }
+
     public void close() {
         try {
             flush(true);
@@ -158,7 +251,9 @@ public class FileBuffer implements Closeable {
                 }
                 channel.close();
             }
-            if (buffer != null && buffer.isDirect()) ((DirectBuffer) buffer).cleaner().clean();
+            if (buffer != null && buffer.isDirect()) {
+                AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> closeDirectByteBufferPrivileged(buffer));
+            }
             if (out != null) out.close();
             channel = null;
             out = null;
